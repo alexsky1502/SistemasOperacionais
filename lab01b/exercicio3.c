00734ec8ca0eeb7087709ac24bb6b338afa22b61
gabrielnov <gabrielcarvalho169@hotmail.com>
2023-02-18 11:23:33 -0300

    adiciona ex 3

---
+++ lab01b/exercicio3.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+    int total = 9, i, j;
+
+    for (i = 1; i <= (total/2) +1; i++){
+        printf("%*s", i, "");
+        for (j = i; j <= (total-i)+1; j++){
+            printf("%d", j);
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
