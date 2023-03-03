48f72ea5546adcde806094cb4923b0121aaa7ef2
Ubuntu <alexsyk1502@gmail.com>
2023-03-03 20:13:15 +0000

    ex1 resolvido

--- LabAula02/ex1.c
+++ LabAula02/ex1.c
@@ -3,7 +3,11 @@
 int main(int argc, char *argv[]){
 	char texto[100];
 	FILE* file;
-	file = fopen("/SistemasOperacionais/LabAula02/entrada.txt");
-	getchar();
+	if (argv[1] == NULL) {
+		printf("colocar o caminho do arquivo entrada.txt");
+	} //"/SistemasOperacionais/LabAula02/entrada.txt"
+	file = fopen(argv[1], "r");
+	fgets(texto, sizeof(texto), file);
+	printf("%s", texto);
 
 }
