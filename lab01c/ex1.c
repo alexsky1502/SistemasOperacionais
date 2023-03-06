d567b402664ca9c75cd56db1f35a769256893b14
Gabriel <gc.novaes@outlook.com>
2023-03-06 11:06:54 -0300

    atualiza modo de uso

--- lab01c/ex1.c
+++ lab01c/ex1.c
@@ -4,7 +4,7 @@ int main(int argc, char *argv[]){
 	char texto[100];
 	FILE* file;
 	if (argv[1] == NULL) {
-		printf("modo de uso: cat /path/to/file");
+		printf("modo de uso: ./cat /path/to/file");
 	} 
 	file = fopen(argv[1], "r");
 	fgets(texto, sizeof(texto), file);
--- lab01c/ex2.c
+++ lab01c/ex2.c
@@ -4,7 +4,7 @@ int main(int argc, char *argv[]){
 	char texto[100];
 	FILE *old, *new;
 	if (argv[1] == NULL || argv[2] == NULL) {
-		printf("modo de uso: cp /path/to/file /path/to/file");
+		printf("modo de uso: ./cp /path/to/file /path/to/file");
 	} 
 	old = fopen(argv[1], "r");
 	fgets(texto, sizeof(texto), old);
