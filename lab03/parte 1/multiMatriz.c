d14a731f7eed39c5a908d3c8f4548f6339ae33bc
Alex SYK <alexsyk1502@gmail.com>
2023-04-06 15:19:19 -0300

    arquivo inicial lab03

---
+++ lab03/parte 1/multiMatriz.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
+
+void *Pth_mat_vect(void* rank){
+    long my_rank = (long) rank;
+    int i, j;
+    int local_m = m/thread_count;
+    int my_first_row = my_rank*local_m;
+    int my_last_row = (my_rank+1)*local_m - 1;
+
+    for(i = my_first_row; i <= my_last_row; i++){
+        y[i] = 0.0;
+        for(j = 0; j < n; j++){
+            y[i] += A[i][j]*x[j];
+        }
+    }
+
+    return NULL;
+} /* Pth_mat_vect */
