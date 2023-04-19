488140135b2bb1ae6a9c99ed20163d102f353147
Gabriel <gc.novaes@outlook.com>
2023-04-19 18:13:23 -0300

    adiciona pi com mutex

---
+++ lab03/parte 2/pi_mutex.c
@@ -0,0 +1,66 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <pthread.h>
+
+long thread_count = 5;
+long long n = 10;
+double sum;
+pthread_mutex_t mutex;
+
+void* Thread_sum(void* rank);
+
+/* Only executed by main thread */
+void Get_args(int argc, char* argv[]);
+void Usage(char* prog_name);
+double Serial_pi(long long n);
+
+int main(int argc, char* argv[]) {
+   long       thread;  /* Use long in case of a 64-bit system */
+   pthread_t* thread_handles;
+
+   thread_handles = (pthread_t*) malloc (thread_count*sizeof(pthread_t)); 
+   pthread_mutex_init(&mutex, NULL);
+   sum = 0.0;
+
+   for (thread = 0; thread < thread_count; thread++)  
+      pthread_create(&thread_handles[thread], NULL,
+          Thread_sum, (void*)thread);  
+
+   for (thread = 0; thread < thread_count; thread++) 
+      pthread_join(thread_handles[thread], NULL); 
+
+   sum = 4.0*sum;
+   printf("com n = %lld termos,\n", n);
+   printf("pi estimado = %.15f\n", sum);
+   
+   pthread_mutex_destroy(&mutex);
+   free(thread_handles);
+   return 0;
+}  /* main */
+
+/*------------------------------------------------------------------*/
+void* Thread_sum(void* rank) {
+   long my_rank = (long) rank;
+   double factor;
+   long long i;
+   long long my_n = n/thread_count;
+   long long my_first_i = my_n*my_rank;
+   long long my_last_i = my_first_i + my_n;
+   double my_sum = 0.0;
+
+   if (my_first_i % 2 == 0)
+      factor = 1.0;
+   else
+      factor = -1.0;
+
+   for (i = my_first_i; i < my_last_i; i++, factor = -factor) {
+      my_sum += factor/(2*i+1);
+   }
+   pthread_mutex_lock(&mutex);
+   sum += my_sum;
+   pthread_mutex_unlock(&mutex);
+
+   return NULL;
+}  
+
