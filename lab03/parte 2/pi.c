9b3e4413080ddd99a3f464c7c98cf7e593e968aa
Gabriel <gc.novaes@outlook.com>
2023-04-20 16:08:29 -0300

    adiciona arquivo 2

--- lab03/parte 2/pi.c
+++ lab03/parte 2/pi.c
@@ -4,9 +4,9 @@
 #include <pthread.h>
 
 long thread_count = 5;
-long long n = pow(10, 8);
+long long n = pow(10,8);
+int flag;
 double sum;
-pthread_mutex_t mutex;
 
 void* Thread_sum(void* rank);
 
@@ -15,13 +15,14 @@ void Usage(char* prog_name);
 double Serial_pi(long long n);
 
 int main(int argc, char* argv[]) {
-   long       thread;  
+   long       thread; 
    pthread_t* thread_handles;
+   double start, finish, elapsed;
 
    thread_handles = (pthread_t*) malloc (thread_count*sizeof(pthread_t)); 
-   pthread_mutex_init(&mutex, NULL);
+   
    sum = 0.0;
-
+   flag = 0;
    for (thread = 0; thread < thread_count; thread++)  
       pthread_create(&thread_handles[thread], NULL,
           Thread_sum, (void*)thread);  
@@ -30,38 +31,34 @@ int main(int argc, char* argv[]) {
       pthread_join(thread_handles[thread], NULL); 
 
    sum = 4.0*sum;
-   printf("mutexes\n");
-   printf("com n = %lld termos,\n", n);
-   printf("com %ld threads,\n", thread_count);
-   printf("pi estimado = %.15f\n", sum);
-   
-   pthread_mutex_destroy(&mutex);
+    printf("busy wait\n");
+    printf("com n = %lld termos,\n", n);
+    printf("com %ld threads,\n", thread_count);
+    printf("pi estimado = %.15f\n", sum);
    free(thread_handles);
    return 0;
-}  /* main */
+}  
 
-/*------------------------------------------------------------------*/
 void* Thread_sum(void* rank) {
    long my_rank = (long) rank;
-   double factor;
+   double factor, my_sum = 0.0;
    long long i;
    long long my_n = n/thread_count;
    long long my_first_i = my_n*my_rank;
    long long my_last_i = my_first_i + my_n;
-   double my_sum = 0.0;
 
    if (my_first_i % 2 == 0)
       factor = 1.0;
    else
       factor = -1.0;
 
-   for (i = my_first_i; i < my_last_i; i++, factor = -factor) {
-      my_sum += factor/(2*i+1);
-   }
-   pthread_mutex_lock(&mutex);
-   sum += my_sum;
-   pthread_mutex_unlock(&mutex);
+   for (i = my_first_i; i < my_last_i; i++, factor = -factor) 
+      my_sum += factor/(2*i+1);  
+   
+   while (flag != my_rank);
+   sum += my_sum;  
+   flag = (flag+1) % thread_count;
 
    return NULL;
-}  
+} 
 
---
+++ lab03/parte 2/readme.md
@@ -0,0 +1,23 @@
+## Com busy wait
+
+```
+busy wait
+com n = 100000000 termos,
+com 2 threads,
+pi estimado = 3.3.141592643589817
+```
+
+## Com mutexes
+```
+mutexes
+com n = 100000000 termos,
+com 2 threads,
+pi estimado = 3.141592643589817
+```
+
+## Valor de pi
+```
+mutexes
+com n = 100000000 termos,
+pi = 3.14159265
+```
