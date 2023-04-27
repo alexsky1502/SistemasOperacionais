d3acf268d3e9ca5564c65ddc7bf3e39e88d6c4b2
Gabriel <gc.novaes@outlook.com>
2023-04-27 11:24:40 -0300

    adiciona novas opcoes

--- lab04/ex1.c
+++ lab04/ex1.c
@@ -1,12 +1,7 @@
-/******************************************************************************
 
-Welcome to GDB Online.
-GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
-C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
-Code, Compile, Run and Debug online from anywhere in world.
-
-*******************************************************************************/
 #include <stdio.h>
+#include <stdlib.h>
+ 
 
 struct reg {
     int conteudo; 
@@ -18,21 +13,58 @@ void imprime(celula *p){
     printf(" %d ", p->conteudo);
     
     if (p->prox == NULL){
+        printf("\n");
         return;
     }
     
     imprime(p->prox);
 }
 
-void printTamanho(celula x){
-    printf("\ntamanho: %ld\n", sizeof(x));
+int tamanho(celula x){
+    return sizeof(x);
 }
 
-void remove()
+void removeItem(celula *head, int valor){
+    printf("removendo item %d\n", valor);
+    
+    celula *atual, *prox;
+    atual = head;
+    while(atual->prox != NULL){
+        prox = atual->prox;
+
+        if (prox->conteudo == valor){
+            atual->prox = prox->prox;
+            printf("item %d removido com sucesso\n", valor);
+            return;
+        }
+        atual = prox;
+    }
+    
+    printf("item %d nao foi encontrado\n", valor);
+}
+
+int maximo(int tamanho){
+    FILE *meminfo = fopen("/proc/meminfo", "r");
+
+    char line[256];
+    while(fgets(line, sizeof(line), meminfo))
+    {
+        int ram;
+        if(sscanf(line, "MemTotal: %d kB", &ram) == 1)
+        {
+            fclose(meminfo);
+           
+            return  ram/tamanho;
+        }
+    }
+
+    fclose(meminfo);
+    return -1;
+}
 
 int main()
 {
-
+    celula head;
     celula a; 
     celula b; 
     celula c; 
@@ -43,8 +75,16 @@ int main()
     a.prox = &b;
     b.prox = &c;
     c.prox = NULL;
+    
+    int tam = tamanho(a);
+    printf("\ntamanho da celula: %d\n", tam);
+    
     imprime(&a);
-    printTamanho(a);
+    removeItem(&a, 2);
+    imprime(&a);
+    
+    int max = maximo(2);
+     printf("maximo de celulas: %d\n", max);
     return 0;
 }
 
